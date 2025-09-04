5a0d04bbde3cce2c988c958d418a425436ca7b12
NelMatrix743 <nelmatrix155@gmail.com>
2025-09-04 18:23:06 +0000

    feat: add color resources

---
+++ lib/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+Color whiteBackground = Color(0xFFFDFFFF);
+Color lightGreen = Color(0xFF33CC68);
+Color deepGreen = Color(0xFF036536);
+
+MaterialColor createMaterialColor(Color color) {
+  List strengths = <double>[0.05];
+  Map<int, Color> swatch = {};
+  final int r = (color.r * 255.0).round(),
+      g = (color.g * 255.0).round(),
+      b = (color.b * 255.0).round();
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  for (var strength in strengths) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  }
+  return MaterialColor(color.toARGB32(), swatch);
+}

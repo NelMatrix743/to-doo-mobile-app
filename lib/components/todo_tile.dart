c34ba54a19b8758911fba6cb57fc939cb367f8a7
NelMatrix743 <nelmatrix155@gmail.com>
2025-09-04 23:34:47 +0000

    feat: implement the todo tile class

--- lib/components/todo_tile.dart
+++ lib/components/todo_tile.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:simple_todo_app/colors.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
+
+  const ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        padding: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: lightGreen,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+            // ignore: unnecessary_this
+            Text(this.taskName),
+          ],
+        ),
+      ),
+    );
   }
 }

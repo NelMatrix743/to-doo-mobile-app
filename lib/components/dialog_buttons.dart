12ea1db7a9aa62f220742e9ba33f3254de8a5646
NelMatrix743 <nelmatrix155@gmail.com>
2025-09-10 08:02:14 +0000

    feat: add base class implementation for dialog buttons

---
+++ lib/components/dialog_buttons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DialogButton extends StatelessWidget {
+  const DialogButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

c99b5c7882fd120ae764b5fb591530a73e752b9a
NelMatrix743 <nelmatrix155@gmail.com>
2025-09-23 20:36:26 +0000

    feat: implement slidable function for deleting todo tasks

--- lib/components/todo_tile.dart
+++ lib/components/todo_tile.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:simple_todo_app/colors.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
   final void Function(bool?)? onChanged;
+  final Function(BuildContext) deleteTaskFunction;
 
   const ToDoTile({
     super.key,
     required this.taskName,
     required this.taskCompleted,
+    required this.deleteTaskFunction,
     this.onChanged,
   });
 
@@ -17,30 +20,42 @@ class ToDoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
-      child: Container(
-        padding: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: lightGreen,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
+      child: Slidable(
+        startActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              activeColor: Colors.white,
-              checkColor: lightGreen,
-            ),
-            // ignore: unnecessary_this
-            Text(
-              taskName,
-              style: TextStyle(
-                decoration: taskCompleted
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-              ),
+            SlidableAction(
+              onPressed: this.deleteTaskFunction,
+              icon: Icons.delete,
+              backgroundColor: Colors.red.shade300,
             ),
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: lightGreen,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.white,
+                checkColor: lightGreen,
+              ),
+              // ignore: unnecessary_this
+              Text(
+                taskName,
+                style: TextStyle(
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

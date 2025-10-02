9b3f79de8fcf954fbe1f8a077554c73cb33ea6b4
NelMatrix743 <nelmatrix155@gmail.com>
2025-10-02 22:38:56 +0000

    feat: implement the todo database handler class

---
+++ lib/data/todo_database.dart
@@ -0,0 +1,32 @@
+import 'package:hive_ce_flutter/hive_flutter.dart';
+
+class ToDoDatabase {
+  List<List<dynamic>> _todoList = [];
+  late Box<List<dynamic>> todoEntryBox;
+
+  Future<List<List<dynamic>>> load() async {
+    todoEntryBox = await Hive.openBox<List<dynamic>>('todo_databox');
+
+    // Load data from the box
+    _todoList = todoEntryBox.values.toList();
+    return _todoList;
+  }
+
+  // Add a new task
+  Future<void> addTask(List<List<dynamic>> task) async {
+    await todoEntryBox.add(task); // Hive auto-generates keys
+    _todoList = todoEntryBox.values.toList(); // keep memory in sync
+  }
+
+  // Update a task by key (index in the box)
+  Future<void> updateTask(int index, List<List<dynamic>> updatedTask) async {
+    await todoEntryBox.putAt(index, updatedTask);
+    _todoList = todoEntryBox.values.toList();
+  }
+
+  // Delete a task by key (index in the box)
+  Future<void> deleteTask(int index) async {
+    await todoEntryBox.deleteAt(index);
+    _todoList = todoEntryBox.values.toList();
+  }
+}

a4282a268bd36d7f0d935e1046bc03f86a2d9dd7
NelMatrix743 <nelmatrix155@gmail.com>
2025-10-02 22:55:37 +0000

    bugfix: change wrong parameter type

--- lib/data/todo_database.dart
+++ lib/data/todo_database.dart
@@ -13,13 +13,13 @@ class ToDoDatabase {
   }
 
   // Add a new task
-  Future<void> addTask(List<List<dynamic>> task) async {
+  Future<void> addTask(List<dynamic> task) async {
     await todoEntryBox.add(task); // Hive auto-generates keys
     _todoList = todoEntryBox.values.toList(); // keep memory in sync
   }
 
   // Update a task by key (index in the box)
-  Future<void> updateTask(int index, List<List<dynamic>> updatedTask) async {
+  Future<void> updateTask(int index, List<dynamic> updatedTask) async {
     await todoEntryBox.putAt(index, updatedTask);
     _todoList = todoEntryBox.values.toList();
   }
